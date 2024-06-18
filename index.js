3113d5d9c94ebfef0df865f53b452964a4939648
Arnab Majumder <arnabmajumdar399@gmail.com>
2024-06-19 03:45:13 +0530

    multer

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ import express from 'express'
 import cors from 'cors'
 import multer from 'multer'
 import { v4 as uuidv4 } from 'uuid'
+import path from 'path'
 
 const app = express()
+
+// multer middlewares
+const storage = multer.diskStorage({
+    destination : function(req , file , cb){
+        cb(null , "./uploads")
+    },
+    filename : function(req , file , cb) {
+        cb(null , file.fieldname + "-" + uuidv4() + path.extname(file.originalname))
+    }
+})
+
+// multer configuration
+const upload = multer({storage : storage})
 
 app.use(
     cors({
@@ -15,16 +29,21 @@ app.use(
 
 app.use((req , res , next) => {
     res.header("Access-Contro;-Allow-Headers" , "Origin , X-Requested-With , Content-Type , Accept")
+    next()
 })
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
-app.use(express.static("uploads"))
+app.use("/uploads" , express.static("uploads"))
 
 app.get('/' , (req , res) => {
     res.json({message: "Hello Brother !!"})
 })
 
+app.post("/upload" , upload.single('file') , function(req , res){
+    console.log("file uploaded")
+})
+
 app.listen(8000 , () => {
-    console.log("Server is running at port 4000...")
+    console.log("Server is running at port 8000...")
 })
